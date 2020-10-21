3ef7e72f007a2a6f3e905dd0d751c5c1fa83c281
ocarvajarh <31353268+ocarvajarh@users.noreply.github.com>
2020-10-21 14:08:45 -0500

    Update Application.java

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -34,7 +34,7 @@ public class Application extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer://foo?period=12000")
+        from("timer://foo?period=8000")
             .setBody().constant("Hello World")
             .log(">>> ${body}");
     }
